c5df72ae63bdd2b153d9842ae9b62a9c6a9d8918
Carolina Acosta <carolinaacosta.n@gmail.com>
2020-03-07 16:19:04 +0000

    Updates

---
+++ precache-manifest.8e86dd8473c20238f2f0299afe215c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9d838fa484782215181d0ff1981621",
+    "url": "/Travel-Survey/index.html"
+  },
+  {
+    "revision": "ecd6eed6f55594f6db45",
+    "url": "/Travel-Survey/static/css/main.04619c9d.chunk.css"
+  },
+  {
+    "revision": "b6332daed97737531490",
+    "url": "/Travel-Survey/static/js/2.d5971143.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Travel-Survey/static/js/2.d5971143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd6eed6f55594f6db45",
+    "url": "/Travel-Survey/static/js/main.638608aa.chunk.js"
+  },
+  {
+    "revision": "45c7363355cc5008f208",
+    "url": "/Travel-Survey/static/js/runtime-main.94df062e.js"
+  }
+]);
